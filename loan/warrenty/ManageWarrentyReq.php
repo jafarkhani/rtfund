a32e54b87248640b0d11a153f59e522005d62edf
mahdipour <mahdipour@172.21.13.13>
2021-07-15 14:05:27 +0430

    Merge origin/master
    Conflicts:
    	office/workflow/TreeProcess.php

--- loan/warrenty/ManageWarrentyReq.php
+++ loan/warrenty/ManageWarrentyReq.php
@@ -12,42 +12,48 @@ require_once 'ManageWarrentyReq.js.php';
 
 $dg = new sadaf_datagrid("WGrid", $js_prefix_address . "ManageWarrentyReq.data.php?task=searchWR", "WDIV");
 
-$dg->addColumn(" شناسه رکورد", "BID", "int",true);
 $dg->addColumn("کد پرسنلی", "PersonID", "int",true);
-$dg->addColumn(" ", "BailType", "int",true); 
-$dg->addColumn(" ", "param1", "int",true); 
-$dg->addColumn(" ", "param2", "int",true); 
-$dg->addColumn(" ", "param3", "int",true); 
-$dg->addColumn(" ", "param4", "int",true); 
-$dg->addColumn(" ", "param5", "int",true); 
-$dg->addColumn(" ", "LetterNo", "int",true); 
+$dg->addColumn("شماره نامه ", "LetterNo", "int",true); 
 
-$col = $dg->addColumn("نام شخص حقیقی/حقوقی", "fullname", "");
+$col = $dg->addColumn("کد ضمانت  نامه", "BID", "int");
+$col->width = 100;
+
+$col = $dg->addColumn("نام شخص حقیقی/حقوقی ", "fullname", "");
 $col->width = 150;
 
 $col = $dg->addColumn("نوع ضمانت نامه ", "BailTypeTitle","string");
-$col->width = 100;
+$col->width = 130;
 
 $col = $dg->addColumn("موضوع قرارداد", "subject","string");
-$col->width = 250;
 
-$col = $dg->addColumn("وضعیت ", "status","string");
-$col->width = 80;
+$col = $dg->addColumn("وضعیت فرم", "status","string");
+$col->width = 100;
+
+$col = $dg->addColumn("وضعیت ضمانت نامه", "ProcessStatus","string");
+$col->width = 110;
 
 $col = $dg->addColumn("عملیات", "", "string");
-$col->renderer = "function(v,p,r){return WelfareCenters.opRender(v,p,r);}";
-$col->width = 80;
+//$col->renderer = "function(v,p,r){return WelfareCenters.opRender(v,p,r);}";
+$col->renderer = "WelfareCenters.OperationRender";
+$col->width = 60;
+
+$dg->addColumn("کد نوع ضمانت نامه ", "BailType", "int",true); 
+$dg->addColumn(" پارامتر1", "param1", "int",true); 
+$dg->addColumn(" پارامتر2", "param2", "int",true); 
+$dg->addColumn(" پارامتر3", "param3", "int",true); 
+$dg->addColumn(" پارامتر4", "param4", "int",true); 
+$dg->addColumn(" پارامتر5", "param5", "int",true); 
+$dg->addColumn(" ", "IID", "int",true); 
 
 $dg->addButton = true;
 $dg->addHandler = "function(){WelfareCentersObject.AddWFC();}";
 
-$dg->pageSize = "20";
-$dg->EnableSearch = false ;
+$dg->pageSize = "15";
 $dg->emptyTextOfHiddenColumns = true;
-$dg->width = 660;
-$dg->height = 430;
-$dg->title = "لیست ضمانت نامه ها";
-$dg->autoExpandColumn = "title";
+$dg->width = 860;
+$dg->height = 520;
+$dg->title = "فهرست ضمانت نامه های درخواستی";
+$dg->autoExpandColumn = "subject";
 $dg->DefaultSortField = "BID"; 
 $dg->DefaultSortDir = "DESC";
 
@@ -63,6 +69,6 @@ $grid = $dg->makeGrid_returnObjects();
 </script>
 <center>
     <div id="ErrorDiv" style="width:40%"></div><br>
-    <form id="mainForm"> <div id="mainpanel2"></div> </form>    <br>
+    <form id="mainForm"> <div id="mainpanel"></div> </form>    <br>
     <div id="WDIV" style="width:100%"></div>
 </center>
