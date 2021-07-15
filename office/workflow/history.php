ae18edad89eb6206e2113fb0ad58382f4099936c
mahdipour <mahdipour@172.21.13.13>
2021-07-15 14:42:48 +0430

    Merge origin/master
    Conflicts:
    	office/dms/dms.class.php
    	office/dms/documents.php

--- office/workflow/history.php
+++ office/workflow/history.php
@@ -11,17 +11,20 @@ require_once inc_component;
 
 if(!empty($_REQUEST["RowID"]))
 {
+	
 	$FlowRowObj = new WFM_FlowRows($_REQUEST["RowID"]);
 	$FlowID = $FlowRowObj->FlowID;
 	$ObjectID = $FlowRowObj->ObjectID;
 }
 else if(!empty($_REQUEST["FlowID"]) && !empty($_REQUEST["ObjectID"]))
-{
+{	
+	
 	$FlowID = $_REQUEST["FlowID"];
 	$ObjectID = $_REQUEST["ObjectID"];
 }
 else if(!empty($_REQUEST["RequestID"]))
 {
+	
 	$ReqObj = new WFM_requests($_REQUEST["RequestID"]);
 	$FlowID = $ReqObj->_FlowID;
 	$ObjectID = $ReqObj->RequestID;
@@ -31,7 +34,7 @@ else
 	 
 $query = "select fr.* ,fs.StepID, fs.IsOuter,bf.*,
 				ifnull(fr.StepDesc, ifnull(fs.StepDesc,'ارسال اولیه')) StepDesc,
-				concat_ws(' ',fname, lname,CompanyName) fullname
+				concat_ws(' ',fname, lname,CompanyName) fullname, f.IsTree
 			from WFM_FlowRows fr
 			join WFM_flows f using(FlowID)
 			join BaseInfo bf on(bf.TypeID=11 AND f.ObjectType=bf.InfoID)
@@ -49,6 +52,8 @@ if(count($Logs) == 0)
 }
 else 
 {
+
+	
 	for ($i=0; $i<count($Logs); $i++)
 	{
 		$backgroundColor = ($i%2 == 1 ? "style='background-color:#efefef'" : "");
@@ -62,7 +67,7 @@ else
 			else if($Logs[$i]["ActionType"] == "REJECT")
 				$StepDesc = "رد " . $StepDesc;
 		}
-		else if($Logs[$i]["ActionType"] == "REJECT")
+		else if($Logs[$i]["ActionType"] == "REJECT" && $Logs[$i]["IsTree"] == "NO" )
 			$StepDesc = "برگشت " . $StepDesc;
 			
 		
@@ -90,35 +95,124 @@ else
 	}
 	if($LastRecord["StepRowID"] != "")
 	{*/
+	
 		$StepID = $LastRecord["StepID"] == "" ? 0 :
 			($LastRecord["ActionType"] == "CONFIRM" ? $LastRecord["StepID"] + 1 : $LastRecord["StepID"] - 1);
+		
+		$WhereClause = "" ; 
+		$whereParam = array();
+		$whereParam[':FID'] =  $FlowID ; 
+		
+		if($Logs[0]["IsTree"] == 'YES')
+		{
+			$WhereClause = " p.PersonID = :PID  " ; 
+			$whereParam[':PID'] =  $_SESSION["USER"]["PersonID"] ; 
+		}
+		else
+		{
+			$WhereClause = " fs.StepID= :SID  " ; 
+			$whereParam[':SID'] =  $StepID ; 
+		}
+		
 		$query = "select StepDesc,PostName,
 					concat_ws(' ',fname, lname,CompanyName) fullname
 				from WFM_FlowSteps fs
 				left join BSC_jobs j on(fs.JobID=j.JobID or fs.PostID=j.PostID)
 				left join BSC_posts ps on(j.PostID=ps.PostID)
 				left join BSC_persons p on(j.PersonID=p.PersonID or fs.PersonID=p.PersonID)
-				where fs.IsActive='YES' AND fs.FlowID=? AND fs.StepID=?";
-		$nextOne = PdoDataAccess::runquery($query, array($FlowID, $StepID));
-
+				where fs.IsActive='YES' AND fs.FlowID=:FID AND ".$WhereClause ;
+		$nextOne = PdoDataAccess::runquery($query, $whereParam);
+		
+		
 		if(count($nextOne)>0)
 		{
-			$str = "";
-			foreach($nextOne as $row)
-				$str .= "<br>" . $row["fullname"] . 
-					($row["PostName"] != "" ? " [ پست : " . $row["PostName"] . " ]" : "") . " و ";
-			$str = substr($str, 0, strlen($str)-3);
-
-			$tbl_content .= "<tr style='background-color:#A9E8E8'>
+			
+			if($Logs[0]["IsTree"] != 'YES') 
+			{
+				$str = "";
+				foreach($nextOne as $row)
+					$str .= "<br>" . $row["fullname"] . 
+						($row["PostName"] != "" ? " [ پست : " . $row["PostName"] . " ]" : "") . " و ";
+				$str = substr($str, 0, strlen($str)-3);
+				$tbl_content .= "<tr style='background-color:#A9E8E8'>
 					<td colspan=4 align=center>در حال حاضر فرم در مرحله <b>" . 
 					$nextOne[0]["StepDesc"] . "</b>  در کارتابل <b>" . $str . "</b><br> می باشد.</td>
 				</tr>";
+
+			}
+			elseif(empty($_REQUEST['TrackHis'])) 
+			{
+				$str = "";				
+				$str .= "<br>" . $nextOne[0]["fullname"] . 
+					($nextOne[0]["PostName"] != "" ? " [ پست : " . $nextOne[0]["PostName"] . " ]" : "") . " و ";
+				$str = substr($str, 0, strlen($str)-3);
+				$tbl_content .= "<tr style='background-color:#A9E8E8'>
+					<td colspan=4 align=center>در حال حاضر فرم در مرحله <b>" . 
+					"بررسی</b>  در کارتابل <b>" . $str . "</b><br> می باشد.</td>
+				</tr>";
+			}
+			else {
+				
+				$query = "  SELECT fs.StepDesc,ps.PostName,
+								   concat_ws(' ',p.fname, p.lname,p.CompanyName) fullname ,
+								   fs2.StepDesc StepDesc2 ,ps2.PostName PostName2,
+								   concat_ws(' ',p2.fname, p2.lname,p2.CompanyName) fullname2, 
+								   fr.IsEnded
+
+
+							FROM WFM_FlowRows fr
+							   LEFT JOIN  WFM_FlowSteps fs ON fr.StepRowID = fs.StepParentID
+							   LEFT JOIN BSC_jobs j ON(fs.JobID=j.JobID or fs.PostID=j.PostID)
+							   LEFT JOIN BSC_posts ps ON(j.PostID=ps.PostID)
+							   LEFT JOIN BSC_persons p ON(j.PersonID=p.PersonID or fs.PersonID=p.PersonID)
+							   
+								LEFT JOIN WFM_FlowSteps fs2 ON fr.StepRowID = fs2.StepRowID  
+								LEFT JOIN WFM_FlowSteps fs3 ON fs2.ReturnStep = fs3.StepRowID  
+								LEFT JOIN BSC_jobs j2 ON(fs3.JobID=j2.JobID or fs3.PostID=j2.PostID)
+								LEFT JOIN BSC_posts ps2 ON(j2.PostID=ps2.PostID)
+								LEFT JOIN BSC_persons p2 ON(j2.PersonID=p2.PersonID or fs3.PersonID=p2.PersonID)
+
+							WHERE fr.FlowID = :FID  and fr.ObjectID = :OID  and fr.IsLastRow = 'YES' and 
+								  if(fs.StepDesc != '' , fs.LastStep = 0 , (1=1) ) " ; 
+				$nextOne = PdoDataAccess::runquery($query , array(":FID" => $FlowID , ":OID" => $ObjectID ) );
+				
+				if($nextOne[0]["IsEnded"] == 'YES' ){
+					$tbl_content .= "<tr style='background-color:#A9E8E8'>
+									<td colspan=4 align=center><b>گردش فرم پایان یافته است.</b></td>
+									</tr>";
+				}
+				else if(!empty($nextOne[0]["fullname"]))
+				{
+					$str = "";				
+					$str .= "<br>" . $nextOne[0]["fullname"] . 
+						($nextOne[0]["PostName"] != "" ? " [ پست : " . $nextOne[0]["PostName"] . " ]" : "") . " و ";
+					$str = substr($str, 0, strlen($str)-3);
+					$tbl_content .= "<tr style='background-color:#A9E8E8'>
+						<td colspan=4 align=center>در حال حاضر فرم در مرحله <b>" . 
+						"بررسی</b>  در کارتابل <b>" . $str . "</b><br> می باشد.</td>
+					</tr>";
+				}
+				else {
+					$str = "";				
+					$str .= "<br>" . $nextOne[0]["fullname2"] . 
+						($nextOne[0]["PostName2"] != "" ? " [ پست : " . $nextOne[0]["PostName2"] . " ]" : "") . " و ";
+					$str = substr($str, 0, strlen($str)-3);
+					$tbl_content .= "<tr style='background-color:#A9E8E8'>
+						<td colspan=4 align=center>در حال حاضر فرم در مرحله <b>" . 
+						"بررسی</b>  در کارتابل <b>" . $str . "</b><br> می باشد.</td>
+					</tr>";
+				}
+		
+			}
+			
+			
+			
 		}
 		else
 		{
 			$tbl_content .= "<tr style='background-color:#A9E8E8'>
 				<td colspan=4 align=center><b>گردش فرم پایان یافته است.</b></td>
-				<tr>";
+				</tr>";
 		}
 	//}
 }
